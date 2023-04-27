40ba4979c271823f5fd5e994484e001ce6aa43f0
Edwin <edgoca2498@gmail.com>
2023-04-27 07:29:28 -0600

    chore: Setup environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
 import svgLoader from 'vite-svg-loader';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
